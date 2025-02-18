8f4dffbd0c34d2a5cb500db9853062a056f7bd1e
= <karinac.telles@gmail.com>
2025-02-18 15:39:59 -0300

    criando o objeto carro

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package src.main;
 
+import src.main.classeobjeto.Carro;
+
 public class Main {
     public static void main(String[]args){
-
+        Carro bydDolphin = new Carro();
+        Carro corolaCross = new Carro();
     }
 }
