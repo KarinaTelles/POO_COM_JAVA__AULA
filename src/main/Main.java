50251cac3e42d37a10cf91dee73be53bb4f2ba6b
= <karinac.telles@gmail.com>
2025-02-18 16:31:08 -0300

    configurando os objetos

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,18 @@ import src.main.classeobjeto.Carro;
 
 public class Main {
     public static void main(String[]args){
-        Carro bydDolphin = new Carro();
-        Carro corolaCross = new Carro();
+        Carro dolphin = new Carro();
+        Carro corollaCross = new Carro();
+
+        dolphin.setAno(2023);
+        dolphin.setMarca("BYD");
+        dolphin.setModelo("Dolphin");
+
+        corollaCross.setAno(2022);
+        corollaCross.setMarca("Toyota");
+        corollaCross.setModelo("Corolla Cross");
+
+        System.out.println(dolphin.toString());
+        System.out.println(corollaCross.toString());
     }
 }
--- src/main/classeobjeto/Carro.java
+++ src/main/classeobjeto/Carro.java
@@ -8,4 +8,28 @@ public class Carro {
     public void acelerar() {
         System.out.println("O carro está acelerando!");
     }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro: {" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ano=" + ano +
+                '}';
+    }
 }
